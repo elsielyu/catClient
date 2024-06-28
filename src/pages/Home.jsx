fd82d5164b6423f3f4650a8cbb458ceff4e413d8
moocowcow <swingcat@gmail.com>
2024-06-28 16:38:35 -0700

    import sass; implement availability; start Checkout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import '../css/Home.scss';
 
 const Home = () => {
-    const [testMessage, setTestMessage] = useState('');
     const [catName, setCatName] = useState('');
     const [catImage, setCatImage] = useState('');
     const [catMessage, setCatMessage] = useState('');
-  
-    const loadTest = async () => {
-      const response = await fetch('http://localhost:9000/graphql', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          query: '{test}',
-        }),
-      });
-      const responseBody = await response.json();
-      setTestMessage(responseBody.data.test);
-      return responseBody.data.test;
-    }
+    const [catAvailable, setCatAvailable] = useState(false);
 
     const loadCat = async () => {
         const response = await fetch('http://localhost:9000/graphql', {
@@ -28,26 +14,34 @@ const Home = () => {
             'content-type': 'application/json',
             },
             body: JSON.stringify({
-            query: '{getCat { name, image, message }}',
+            query: '{getCat { name, image, message, available }}',
             }),
         });
         const responseBody = await response.json();
         setCatName(responseBody.data.getCat.name);
         setCatImage(responseBody.data.getCat.image);
         setCatMessage(responseBody.data.getCat.message);
+        setCatAvailable(responseBody.data.getCat.available);
         return responseBody.data.getCat.message;
     };
+
+    useEffect(() => {
+        return loadCat();
+    }, []);
     
     return (
         <>
-            <button onClick={loadTest}>click me</button>
-            <div>{testMessage}</div>
-            <button onClick={loadCat}>click me for cat</button>
-            <div>{catName}</div>
-            <img src={`data:image/jpeg;base64,${catImage}`} alt=""></img>
-            <div>{catMessage}</div>
+            <h1>{catName}</h1>
+            {catAvailable && 
+                <div class="entry">
+                    <span class="badge">Available</span>
+                    <img src={`data:image/jpeg;base64,${catImage}`} alt=""></img>
+                </div>
+            }
+            {!catAvailable && <img src={`data:image/jpeg;base64,${catImage}`} alt=""></img>}
+            <div class="message">{catMessage}</div>
         </>
     );
-}
+};
 
 export default Home;
